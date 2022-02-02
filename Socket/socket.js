b3aeccbb38f3ee7ecaf6fc736f6969e32561bd13
jurniores <65627649+jurniores@users.noreply.github.com>
2022-02-02 15:27:14 -0300

    Add files via upload

--- Socket/socket.js
+++ Socket/socket.js
@@ -5,68 +5,207 @@ module.exports = class Server {
 
     constructor(socket){
         this.socket = socket;
-        this.rinfo = false
-        this.msg = false
-        this.client = []
-        
-        
+        this.rinfo = false;
+        this.msg = false;
+        this.client = [];
+        this.stateGame = [];
+        this.rooms = [];
+ 
     }
-    //Listen event
+  
+    //Ouve os eventos
     On(valida, call){
         let string = this.msg.toString()
         let novaMsg = string.split(':')
-
+        let firstMsg = novaMsg[0];
+        let secondMsg = novaMsg.splice(1,novaMsg.length-1).join(":");
+        
         if(valida == novaMsg[0]) {
             
-            call({port:novaMsg[0], msg:novaMsg[1]});
+            call({port:firstMsg, msg:secondMsg});
+        }
+
+    }
+    //Adiciona um estado fixo para ser enviado sempre
+    AddState(msgs){
+
+        try{
+            let idMsg = JSON.parse(msgs);
+        
+            this.stateGame.map((valor,index)=>{
+                
+                if(idMsg && valor.name == idMsg.name){
+                    idMsg.name = valor.name
+                    idMsg.room = valor.room
+                    this.stateGame[index] = idMsg;
+                }
+            })
+        }catch(e){Error(e)}
+        
+        
+            
+    }
+    //Envia para todos menos para mim
+    SendNotMe(send, msg){
+        try{
+            var existe = this.client.filter(valor=>{
+                if(valor.port === this.rinfo.port && valor.address === this.rinfo.address){
+                    return valor;
+                }
+            })
+    
+            
+    
+            if(existe[0].room){
+                
+                this.client.map(valor=>{
+                    //Valida se tiver uma sala envia a mensagem para aquela sala.
+                    if(valor.room === existe[0].room && valor.port != this.rinfo.port && valor.address != this.rinfo.address) {
+                        this.socket.send(`${send}:${msg}`, valor.port, valor.address)
+                        
+                    }
+                    
+                })
+            }else{
+                this.client.map(valor=>{
+                    //Valida se tiver uma sala envia a mensagem para aquela sala.
+                    if(valor.port != this.rinfo.port && valor.address != this.rinfo.address && !valor.room) {
+                        this.socket.send(`${send}:${msg}`, valor.port, valor.address)
+                        
+                    }
+    
+                })
+            }
+        }catch(e){Error(e)}
+       
+        
+    }
+    SendForMe(send, msg){
+        
+        
+                this.socket.send(`${send}:${msg}`, this.rinfo.port, this.rinfo.address)
+            
+    }
+
+    //Envia para todos conectados ou se tiverem em uma sala envia para a sala
+    Send(send, msg){
+        try{
+            var existe = this.client.filter(valor=>{
+                if(valor.port === this.rinfo.port && valor.address === this.rinfo.address){
+                    return valor;
+                }
+            })
+    
+            
+            if(existe && existe[0].room != null){
+                        
+    
+                        
+                this.client.map(valor=>{
+                    
+                    
+                    //Valida se tiver uma sala envia a mensagem para aquela sala.
+                    if(valor.room === existe[0].room) {
+                        this.socket.send(`${send}:${msg}`, valor.port, valor.address)
+                        
+                    }
+                    
+                })
+            }else{
+                this.client.map(valor=>{
+                    //Valida se tiver uma sala envia a mensagem para aquela sala.
+                    
+                        !valor.room&&this.socket.send(`${send}:${msg}`, valor.port, valor.address)
+                        
+                    
+                
+                })
+            }
+    
+            
+        }catch(e){Error(e)}
+        
+
+        
+        
+    }
+    //Envia o estado fixo para todos
+    SendReliable(send){
+        
+        
+        try{
+            if(this.stateGame.length>0){
+
+                var existe = this.client.filter(valor=>{
+                    if(valor.port === this.rinfo.port && valor.address === this.rinfo.address){
+                        return valor;
+                    }
+                })
+        
+                
+                
+                if(existe && existe[0].room != null){
+                    
+
+                    
+                    this.client.map(valor=>{
+                        
+                        let json = JSON.stringify({Items:this.stateGame.filter(valor=>valor.room==existe[0].room)});
+                        
+                        //Valida se tiver uma sala envia a mensagem para aquela sala.
+                        if(valor.room === existe[0].room) {
+                            this.socket.send(`${send}:${json}`, valor.port, valor.address)
+                            
+                        }
+                        
+                    })
+                }else{
+                    this.client.map(valor=>{
+                        //Valida se tiver uma sala envia a mensagem para aquela sala.
+                        if(!valor.room) {
+                            let json = JSON.stringify({Items:this.stateGame});
+                            this.socket.send(`${send}:${json}`, valor.port, valor.address)
+                            
+                        }
+                        
+                            
+                        
+                    
+                    })
+                }
+    
+                
+                
+            }
+        }catch(e){
+            Error(e);
         }
         
-    }
+      
 
-    SendNotOnce(send, msg){
-        this.client.map(valor=>{
-            //Valida se tiver uma sala envia a mensagem para aquela sala.
-            if(valor.room) {
-                this.socket.send(`${valor.room}:${msg} (${send})`, valor.port)
-            }
-            if(valor.port != this.rinfo.port){
-                this.socket.send(`${send}:${msg}`, valor.port)
-            }
-            
-        })
-    }
-
-    //Send message
-    Send(send, msg){
-        this.client.map(valor=>{
-            //Valida se tiver uma sala envia a mensagem para aquela sala.
-            if(valor.room) {
-                this.socket.send(`${valor.room}:${msg} (${send})`, valor.port)
-            }
-            this.socket.send(`${send}:${msg}`, valor.port)
-        })
+        
         
     }
-    //Send Broadcast
+    
+    //Envia para todos em qualquer sala
     BroadCast(msg){
         this.client.map(valor=>{
             this.socket.send(`brc:${msg} (BroadCast)`, valor.port)
         })
         
     }
-    //Connect and DISCONNECT clients
-    ConnectedUpdate(msg, rinfo, time=0.5){
-        this.rinfo = rinfo;
-        this.msg = msg;
 
-
+    //Conecta e disconecta os clientes, sempre verificando, quando for inserir o nome no cliente sempre chamar este método
+    ConnectedUpdate(infoPlayer,time=0.3){
+        
+        
         //Validação se está logado no servidor
         const date = new Date()
         
         //Setando as configurações dos Clientes
         if(this.client.length === 0){
-            this.client = [{port:rinfo.port,address:rinfo.address, hour:date, room:false}]
-            
+            this.client = [{port:this.rinfo.port,address:this.rinfo.address, hour:date, room:false, name:false, stateGame:false, move:false, valida: 0}]
+           
         }
         const arrayValida =[]
         var port = -1
@@ -78,55 +217,148 @@ module.exports = class Server {
             const dataTimeAfk = (dataMenor/1000)/60
             
             
-            
-            if(valor.port === rinfo.port && valor.address === rinfo.address){
-                
+            //Se o player já existe
+            if(valor.port === this.rinfo.port && valor.address === this.rinfo.address){         
                 arrayValida.push(1);
                 valor.hour = date
-    
+
+                var configPlayer = infoPlayer?JSON.parse(infoPlayer):null;
+                if(infoPlayer && configPlayer.name){
+                    valor.name = configPlayer.name
+                    configPlayer.room = valor.room
+                    this.stateGame.push(configPlayer)
+                    
+                }
+                
+                
             }
             
             if(dataTimeAfk>=time){      
                 port = index
-                
-            
             }
+            
         })
 
 
-
         if(port>=0){
-            this.client.splice(port,1)
+            
+            //Decrementando de quem saiu da sala e se tiver nenhum player exclui a sala do array
+            this.rooms.map((valor2,index)=>{
+                if(this.client[port].room===valor2.room){
+                    valor2.qtdPlayer=valor2.qtdPlayer-1;
+                    valor2.qtdPlayer==0&&this.rooms.splice(index,1);
+                } 
+            });
+            this.stateGame.map((valor,index)=>{
+                if(valor.name == this.client[port].name) this.stateGame.splice(index,1);
+            });
+            this.client.splice(port,1);
+            console.log('Excluiu')
+            
+            
+
             
         }
 
         if(arrayValida.length===0){
 
-            console.log("Arrey valida zerou")
-            this.client = [...this.client, {port: rinfo.port, address:rinfo.address, hour:date,room:false}]
+            
+                this.client = [...this.client, {port: this.rinfo.port, address:this.rinfo.address, hour:date,room:false, name:false, stateGame:false, move:false, valida:0}]
+                //name&&(this.stateGame = [{name:name}]);
+                
+            
             
         }
          
     }
-      //Join room OOR party 
+   
+      //Entra em uma sala
     Join(room){
+        var validaSala = [];
+        //Forma uma nova sala
+        var ranges = Math.floor((Math.random()*10000));
+        //Transforma o json enviado do client em objeto js
+        var sala = JSON.parse(room);
+
+        //filtro para verificar se a sala está cheia
+        var filtro = this.rooms.filter(valor=>{
+            if(valor.room == sala.room && valor.qtdPlayer==valor.maxPlayer){
+                return valor;
+            }
+        })
+
+        //verifica se realmente está cheia se estiver não anexa a sala ao cliente que pediu para entrar
+        if(filtro.length>0) return false;
         
-       this.client.map(valor=>{
-        if(valor.port === this.rinfo.port && valor.address === this.rinfo.address){
-           valor.room = room
+        //Verifica se já existe essa sala nos players
+        if(this.client.filter(valor=>valor.room === ranges).length==0){
+            //Transforma em objeto Javascript o json enviado
+            
+            
+            
+            
+            //se o player que chamou não existir sala ele anexa uma sala ele, que criou ou que entrou
+            this.client.map((valor)=>{
+                if(valor.port === this.rinfo.port && valor.address === this.rinfo.address && !valor.room){
+                   
+                   
+                   if(sala.room==0){
+                        valor.room = sala.room>0?sala.room:ranges
+                        this.rooms.push({name:sala.name,qtdPlayer:1, room:ranges, maxPlayer:sala.maxPlayer>6?6:sala.maxPlayer});
+                        this.stateGame.map((valor2,index)=>{
+                           if(valor2.name==valor.name){
+                               valor2.room = ranges;
+                           }
+                        })
+                   }
+                   //Se existir sala ele verifica se o estado do player ainda não existe sala e insere sala no estado dele
+                   else {
+                       //Verifica se a sala ainda existe, ou se foi destruída, evitando bug de entrar em uma sala fantasma
+                    var room = this.rooms.filter(valor=>valor.room==sala.room)
+                    if(room.length==0) return validaSala.push(1);
+
+                    valor.room = sala.room>0?sala.room:ranges
+                        
+                       //Verifica se o estado do player já tem sala se existir ele insere um numero no array valida para que não haja soma na sala
+                    this.stateGame.map((valor2,index)=>{
+                        if(valor2.name==valor.name){
+                             valor2.room = valor.room;
+                        }
+                    
+                     })
+                     room[0].qtdPlayer++;
+
+                   }
+
+                }
+
+            })
+            
+            if(validaSala.length>0){return false} else{return true}
+               
         }
-       })
+            
+        
+        
+        
+       
     }
 
-    //leave Room
+    //Sai da sala
     Leave(){
         this.client.map(valor=>{
             if(valor.port === this.rinfo.port && valor.address === this.rinfo.address){
                valor.room = false
+               this.rooms.map((valor2,index)=>{
+                   if(valor.room===valor2.room){
+                       valor2.qtdPlayer=valor2.qtdPlayer-1;
+                       valor2.qtdPlayer==0&&this.rooms.splice(index,1);
+                   } 
+               });
             }
            })
     }
-    
+    //disconecta o cliente
     Diconnected(){
         
         this.client.map((valor,index)=>{
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const dgram = require("dgram");
+const { join } = require("path");
 const server = dgram.createSocket("udp4");
+const app = require("express")()
+
+var playersServer = []
 
 const Server = require("./Socket/socket");
 
@@ -12,18 +16,112 @@ server.on('error', (err) => {
     server.close();
   });
 
+
+  //Escutando o cliente
 server.on("message",(msg,rinfo)=>{
-  console.log("ola");
-  socket.ConnectedUpdate(msg,rinfo,1)
+  //atualizando sempre os clientes novos que enviam mensagens
   
+  //Atualiza sempre a mensagem e o ip que é enviado a cada request
+  socket.rinfo = rinfo;
+  socket.msg = msg;
   
-  socket.On("chat",({port,msg})=>{
-    socket.SendNotOnce("chat",msg);
-    console.log(socket.client)
+
+  //Cria sala para jogador
+  socket.On("sala",({port,msg})=>{
+
+    
+    //caso exista algum argumento o jogador entrará em uma sala já criada, senão criará uma nova
+    if(socket.Join(msg)){
+      socket.Send("rcEntraSala", "")
+      socket.Send("rcSalas", JSON.stringify({Items:socket.rooms}))
+    }
+    
+
+  })
+  socket.On("salas",({port,msg})=>{
+
+    //console.log(socket.stateGame) 
+    socket.SendForMe("rcSalas", JSON.stringify({Items:socket.rooms}))
+    
+
+  })
+  //ping é enviado todas as mensagens reliable para todos os usuários a cada tempo de ping que você configurou no cliente
+  socket.On("ping",({port,msg})=>{
+    socket.ConnectedUpdate()
+    socket.Send("ping",msg);
+    
+    console.log(socket.rooms)
+    
+    
+    
+  })
+
+
+
+  socket.On("ataque",({port,msg})=>{
+    console.log(msg)
+    socket.AddState(msg);
+    socket.SendReliable("realible","ola")
+    
+    
+    
+  })
+
+  
+  socket.On("move",({port,msg})=>{
+
+    
+    socket.Send("rcMove",msg);
+    console.log("ola")
+    
+    
+
+  })
+
+  
+  socket.On("pulo",({port,msg})=>{
+    console.log("pulou")
+    socket.Send("rcPulo",msg)
+    
+
   })
   
-})
+  
+  socket.On("login",({port,msg})=>{
+    
+    //adicionando estado inicial
+    
+     //inserindo o nome do player e validando se existe algum nome
+     
+     if(socket.client.filter(valor=>valor.name==msg).length==0){
+      socket.ConnectedUpdate(msg);
+      socket.SendForMe("entra","mensagem para entrar")
+      playersServer = [];
+      for(var i = 0;i<socket.stateGame.length;i++){
+        console.log(socket.stateGame[i].name)
+        
+        playersServer.push(socket.stateGame[i].name)
+        
+      }           
+      
+     }
+     
 
+  })
+
+  socket.On("loginMapa",({port,msg})=>{
+
+    socket.SendReliable("logados");
+
+
+  })
+ 
+
+ 
+
+  
+  
+})
 
   server.on('listening', () => {
     const address = server.address();
@@ -33,3 +131,19 @@ server.on("message",(msg,rinfo)=>{
   
   
   server.bind(3000);
+
+
+
+  //Servidor WEB
+
+  app.get("/",(req,res)=>{
+    let name = "<h4>Players logado:</h4>";
+    for(var i = 0;i<playersServer.length;i++){
+      name+="<li>"+playersServer[i]+"</li>";
+      
+    }
+    res.send("<h1>Quantidade de players online "+playersServer.length+"</h1>  "+name)
+    
+  })
+
+  app.listen(3000,()=>console.log("Servidor TCP rodando na porta 3000"))
